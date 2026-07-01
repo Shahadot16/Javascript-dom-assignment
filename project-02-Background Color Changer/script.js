784a8e4fe6a65d18757a15d5f43644f24e12a9e3
Md. Shahadot Hossain <shahadotraj1@gmail.com>
2026-07-01 15:44:50 +0600

    project 2

---
+++ project-02-Background Color Changer/script.js
@@ -0,0 +1,4 @@
+//banckground color changer
+const colorInput = document.getElementById('colorInput');
+const changeColorButton = document.getElementById('changeColorButton');
+const colorValue = document.getElementById('colorValue');
