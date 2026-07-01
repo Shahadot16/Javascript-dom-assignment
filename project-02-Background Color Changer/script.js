adf71c113ade6260de2ee4a1b63118ae58155b21
Md. Shahadot Hossain <shahadotraj1@gmail.com>
2026-07-01 19:31:05 +0600

    project 02

--- project-02-Background Color Changer/script.js
+++ project-02-Background Color Changer/script.js
@@ -1,4 +1,21 @@
-//banckground color changer
-const colorInput = document.getElementById('colorInput');
-const changeColorButton = document.getElementById('changeColorButton');
-const colorValue = document.getElementById('colorValue');
+const changeColorButton = document.getElementById("change-color-btn");
+const colorValue = document.getElementById("color-value");
+
+function generateRandomColor() {
+  const letters = "0123456789abcdef";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    color += letters[randomIndex];
+  }
+
+  return color;
+}
+
+changeColorButton.addEventListener("click", function () {
+  const randomColor = generateRandomColor();
+
+  document.body.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+});
